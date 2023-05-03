aa9c6f7bf8818474bcaa65e1b32c3fee6d89c34a
Hugo Trippaers <hugo@trippaers.nl>
2023-05-03 21:41:30 +0200

    More updates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::io::{BufRead, Read, Write};
+use crate::OpCodes::{ERRO, SBEGIN, SDATA, SEND, SRSP, UNKN};
 use clap::Parser;
-use std::time::Duration;
-use serialport::{DataBits, Parity, StopBits};
-use retry::{retry};
 use retry::delay::Fixed;
-use crate::OpCodes::{ERRO, SBEGIN};
+use retry::retry;
+use serialport::{DataBits, FlowControl, Parity, StopBits};
+use std::fs::File;
+use std::io::{BufRead, Read, Write};
+use std::time::Duration;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,12 +20,27 @@ struct Args {
 }
 
 #[repr(u8)]
+#[derive(PartialEq, PartialOrd)]
 enum OpCodes {
- SBEGIN = 0x01,
- SDATA  = 0x02,
- SRSP  =  0x03,
- SEND =   0x04,
- ERRO  =  0x05 ,
+    SBEGIN = 0x01,
+    SDATA = 0x02,
+    SRSP = 0x03,
+    SEND = 0x04,
+    ERRO = 0x05,
+    UNKN = 0xFF,
+}
+
+impl From<u8> for OpCodes {
+    fn from(orig: u8) -> Self {
+        match orig {
+            0x01 => return SBEGIN,
+            0x02 => return SDATA,
+            0x03 => return SRSP,
+            0x04 => return SEND,
+            0x05 => return ERRO,
+            _ => return UNKN,
+        };
+    }
 }
 
 fn main() {
@@ -38,18 +54,24 @@ fn main() {
     port.set_data_bits(DataBits::Eight)
         .expect("Failed to set databits");
 
-    port.set_parity(Parity::None)
-        .expect("Failed to set parity");
+    port.set_parity(Parity::None).expect("Failed to set parity");
 
     port.set_stop_bits(StopBits::One)
         .expect("Failed to set stopbits");
+
+    port.write_data_terminal_ready(true)
+        .expect("DTR high failed");
+
+    let mut file = match File::open(&args.firmware) {
+        Err(why) => panic!("couldn't open {}: {}", args.firmware.to_str().unwrap(), why),
+        Ok(file) => file,
+    };
 
     let mut serial_buf: Vec<u8> = vec![0; 1000];
 
     serial_buf[0] = SBEGIN as u8;
     serial_buf[1] = 0;
-    let bytes_written = port.write(&serial_buf[0..2])
-        .expect("Write failed");
+    let bytes_written = port.write(&serial_buf[0..2]).expect("Write failed");
     println!("Bytes written {bytes_written}");
 
     let result = retry(Fixed::from_millis(1000).take(15), || {
@@ -62,17 +84,64 @@ fn main() {
                 if serial_buf[0] == ERRO as u8 {
                     return Err("Programmer returned error".to_owned());
                 }
-                result.unwrap()
-            },
-            Err(err) => Err(err)
-        }
+                Ok(result.unwrap())
+            }
+            Err(err) => Err(err.to_string()),
+        };
     });
 
-    if result.is_ok() {
-        let bytes_read = result.unwrap();
-        println!("Bytes read: {bytes_read}")
-    } else {
-        println!("Timeout reading from serial")
+    if !result.is_ok() {
+        println!("Timeout reading from serial");
+        return;
     }
 
+    // We should have the received byte in the serial buffer now
+    let response: OpCodes = serial_buf[0].into();
+    if response != SRSP {
+        println!("Invalid reponse {:?}", response as u8);
+    }
+
+    println!("Received SRSP, starting upload");
+
+    let mut data_buf: Vec<u8> = vec![0; 515];
+    data_buf[0] = SDATA as u8;
+    let result = file.read(&mut data_buf[1..513]);
+    match result {
+        Ok(count) => {
+            if count < 512 {
+                println!("data underrun on the file")
+            }
+        }
+        Err(err) => panic!("die die die"),
+    }
+    let result = port.write(data_buf.as_slice());
+    match result {
+        Ok(count) => {
+            if count < 515 {
+                println!("not all bytes written to port")
+            }
+        }
+        Err(err) => panic!("die die die"),
+    }
+
+    let result = retry(Fixed::from_millis(1000).take(15), || {
+        let result = port.read(serial_buf.as_mut_slice());
+        return match result {
+            Ok(n) => {
+                if n == 0 {
+                    return Err("Serial connection closed".to_owned());
+                }
+                if serial_buf[0] == ERRO as u8 {
+                    return Err("Programmer returned error".to_owned());
+                }
+                Ok(result.unwrap())
+            }
+            Err(err) => Err(err.to_string()),
+        };
+    });
+
+    if !result.is_ok() {
+        println!("Timeout waiting for SRSP");
+        return;
+    }
 }
